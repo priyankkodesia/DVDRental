4ffd54796751107c3e0d7bd3c3f9687e8f3ba6dc
Vishad Saxena <vishad.saxena@gmail.com>
2020-01-14 10:22:36 +0530

    rating filter

--- src/main/java/com/example/DVDRental/controller/FilmController.java
+++ src/main/java/com/example/DVDRental/controller/FilmController.java
@@ -10,10 +10,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/api")
@@ -26,17 +30,20 @@ public class FilmController {
     private FilmService filmService;
 
     @GetMapping(value = "/films")
-    public ResponseEntity<?> getAllFilms(final HttpServletRequest request) {
+    public ResponseEntity<?> getAllFilms(final HttpServletRequest request,@RequestParam(value = "rating", required = false) String Rating) {
 
         try {
             List<Film> films = filmService.getAllFilms();
+            if(!StringUtils.isEmpty(Rating)){
+            films = films.stream().filter(t->Rating.equalsIgnoreCase(t.getRating())).collect(Collectors.toList());
+            }
             return Responses.createResponse(RequestMethod.GET, HttpStatus.OK, new RestResponse<>(films));
 
         } catch (Exception e) {
             LOG.error("Exception while fetching Clients.Please check the logs for more details.", e);
             return Responses.createResponse(RequestMethod.GET, HttpStatus.INTERNAL_SERVER_ERROR,
                     new RestResponse<String>("Failed to load the Clients!."));
-        }
+        } 
 
     }
 
