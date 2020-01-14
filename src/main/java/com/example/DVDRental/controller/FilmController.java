11e86ad71a60277125b3249e3cb3f1b2e439e297
Vishad Saxena <vishad.saxena@gmail.com>
2020-01-14 10:31:00 +0530

    sorted list of films by year released

--- src/main/java/com/example/DVDRental/controller/FilmController.java
+++ src/main/java/com/example/DVDRental/controller/FilmController.java
@@ -33,10 +33,13 @@ public class FilmController {
     public ResponseEntity<?> getAllFilms(final HttpServletRequest request,@RequestParam(value = "rating", required = false) String Rating) {
 
         try {
+        	LOG.debug("Getting all the films");
             List<Film> films = filmService.getAllFilms();
             if(!StringUtils.isEmpty(Rating)){
+            	LOG.debug("Getting films with rating : " + Rating);
             films = films.stream().filter(t->Rating.equalsIgnoreCase(t.getRating())).collect(Collectors.toList());
             }
+            films.sort((o1,o2)->o1.getReleaseYear().compareTo(o2.getReleaseYear()));
             return Responses.createResponse(RequestMethod.GET, HttpStatus.OK, new RestResponse<>(films));
 
         } catch (Exception e) {
